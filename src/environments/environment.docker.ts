a09746cc6af0b2d1e3ea4e24d48a4f766b432e19
Tristan <russon77@gmail.com>
2018-06-15 15:12:08 -0400

    feat: docker config

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    api: 'https://ssapi.mahasamatman.me',
+    whitelistedDomains: ['ssapi.mahasamatman.me'],
+    blacklistedRoutes: ['https://ssapi.mahasamatman.me/users/login', 'https://ssapi.mahasamatman.me/users/refresh']
+};
